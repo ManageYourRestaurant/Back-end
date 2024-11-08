0328d18a43cdae3ad60a1c6bf9f2a2366f809716
RGBRaccoon <chldusgh0497@gmail.com>
2024-11-08 12:31:43 +0900

    hot-fix

--- model/restorant_model.py
+++ model/restorant_model.py
@@ -6,5 +6,5 @@ from config.db_config import Base
 
 class RestorantModel(Base):
     __tablename__ = "restorant"
-    restorant_id: Mapped[str] = mapped_column(String, default=uuid.uuid4)
+    restorant_id: Mapped[str] = mapped_column(String, primary_key=True, default=uuid.uuid4)
     name: Mapped[str] = mapped_column(String)
